e8cc90254e79aabb8af49a6939d9ad3b96c13991
Andres Lopez <andres_lopez_rm@hotmail.com>
2022-05-09 15:43:08 -0500

    Add env variables to server

--- server/index.js
+++ server/index.js
@@ -2,18 +2,20 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const sdk = require('node-appwrite');
+require('dotenv').config()
+
+console.log(process.env.APPWRITE_ENDPOINT)
 
 const { Query  } = sdk;
 
 const client = new sdk.Client();
-const health = new sdk.Health(client);
 const database = new sdk.Database(client);
 const users = new sdk.Users(client);
 
 client
-    .setEndpoint('https://8080-appwrite-integrationforg-l4agqveejyf.ws-us44.gitpod.io/v1')
-    .setProject('hackaton-appwrite')
-    .setKey('2b227380754f53aad65be07299272d658ab53f4c9c7e5e9aa02f3cce30afb2c3966c8283db66806ce5dfca463f616b9a7148d82eecde0f8fa44094b72ac3779bfc1290ad5e67d38ddc0b58d71afd4a56366e607a7be578f2edb756046e16753bd585c9f4e81fcaa1dfd7d79e605967e0522e3c1407ddb99501993c3d0d4c49a7')
+    .setEndpoint(process.env.APPWRITE_ENDPOINT)
+    .setProject(process.env.APPWRITE_PROJECT)
+    .setKey(process.env.APPWRITE_KEY)
 ;
 
 app.use(cors());
@@ -56,6 +58,6 @@ app.get('/user/:id', async function(req, res) {
 });
 
 
-app.listen(4000, () => {
-  console.log(`Listening to requests on http://localhost:4000`);
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Listening to requests`);
 });
